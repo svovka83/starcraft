9c33e5c56473c96366719171f9ed021b9ba5f8f7
Volodymyr Smoliak <svovka83@gmail.com>
2024-09-12 03:26:17 +0300

    deploy

--- store/functions-ai-logic/logic-ai.ts
+++ store/functions-ai-logic/logic-ai.ts
@@ -33,11 +33,120 @@ export function logicAI(state: GameState, get: () => GameState) {
   const battleUnitId = isUnitBattleId(state.two.battleground.length);
 
   // **********************
+  // DEFENDER AI worker
+  // **********************
+  // **********************
+  // if need fight unit down
+  // **********************
+  if (
+    state.two.worker.length < 3 &&
+    state.one.fighterDown.name &&
+    state.two.fighterDown.name &&
+    state.two.mana >= state.two.fighterDown.mana
+  ) {
+    const functionAI = () => get().fightUnitDown();
+    const AI = functionAI();
+    return { AI };
+  }
+  // **********************
+  // **********************
+  // if need save workers
+  // **********************
+  if (
+    state.two.worker.length < 3 &&
+    state.one.fighterDown.name &&
+    !state.two.fighterDown.name &&
+    battleUnitId !== 0
+  ) {
+    const functionAI = () => get().moveUnitDown(battleUnitId);
+    const AI = functionAI();
+    return { AI };
+  }
+  // **********************
+  // **********************
+  // if minerals finishing
+  // **********************
+  if (state.two.minerals < 3) {
+    const randomFunction = [get().createWorker, get().addMinerals];
+    const randomIndex = Math.floor(Math.random() * randomFunction.length);
+    const functionAI = randomFunction[randomIndex];
+    const AI = functionAI();
+    return { AI };
+  }
+  // **********************
+
+  // **********************
+  // ATTACKER AI
+  // **********************
+  // **********************
+  // if way to boss is empty
+  // **********************
+  if (
+    !state.one.fighterUp.name &&
+    state.two.fighterUp.name &&
+    state.two.mana >= state.two.fighterUp.mana
+  ) {
+    const functionAI = () => get().fightBoss();
+    const AI = functionAI();
+    return { AI };
+  }
+  // **********************
+  // **********************
+  // if unit up is strongest
+  // **********************
+  if (
+    state.one.fighterUp.name &&
+    state.two.fighterUp.name &&
+    state.two.fighterUp.attack >= state.one.fighterUp.health &&
+    state.two.mana >= state.two.fighterUp.mana
+  ) {
+    const functionAI = () => get().fightUnitUp();
+    const AI = functionAI();
+    return { AI };
+  }
+  // **********************
+
+  // **********************
+  // DEFENDER AI BOSS
+  // **********************
+  // **********************
+  // if need defend low boss "finish step"
+  // **********************
+  if (state.two.boss < 10 && !state.two.fighterUp.name && battleUnitId !== 0) {
+    const functionAI = () => get().moveUnitUp(battleUnitId);
+    const AI = functionAI();
+    return { AI };
+  }
+  // **********************
+  // **********************
+  // if need defend low boss "start step"
+  // **********************
+  if (state.two.boss < 10 && !state.two.fighterUp.name && battleUnitId === 0) {
+    const randomFunction = [() => get().buyUnit(unitIdRandom)];
+    const randomIndex = Math.floor(Math.random() * randomFunction.length);
+    // here must be push one more function (comeback unitDown - need create)
+    // here must be push one more function (comeback unitDown - need create)
+    // here must be push one more function (comeback unitDown - need create)
+    const functionAI = randomFunction[randomIndex];
+    const AI = functionAI();
+    return { AI };
+  }
+  // **********************
+
+  // **********************
   const randomFunction = [
     () => get().buyUnit(unitIdRandom),
     get().createWorker,
     get().addMinerals,
   ];
+  // **********************
+
+  // **********************
+  // if minerals too much
+  // **********************
+  if (state.two.minerals > 15) {
+    randomFunction.pop();
+  }
   // **********************
 
   // check for moveUnitUp
