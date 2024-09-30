a1dc8e62629cff4acf27cdbb35d8eda773437ef9
Volodymyr Smoliak <svovka83@gmail.com>
2024-09-30 14:26:05 +0300

    deploy

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -4,8 +4,9 @@ import { NextResponse } from "next/server";
 export async function GET() {
   try {
     const users = await prisma.user.findMany();
+    const numberOfUsers: number = users.length;
 
-    return NextResponse.json(users.length, { status: 200 });
+    return NextResponse.json(numberOfUsers, { status: 200 });
   } catch (error) {
     console.log("[GET_USERS]", error);
     return NextResponse.json({ message: "Server error" }, { status: 500 });
